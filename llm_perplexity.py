0ed2d2c7d296da0dceef04be6ef02994e6eb992a
hex <hexaust@gmail.com>
2024-12-16 10:04:08 +0200

    Add llama-3.3-70b-instruct support

--- llm_perplexity.py
+++ llm_perplexity.py
@@ -14,6 +14,7 @@ def register_models(register):
     register(Perplexity("llama-3.1-sonar-large-128k-chat"), aliases=("sonar-large-chat",))
     register(Perplexity("llama-3.1-70b-instruct"))
     register(Perplexity("llama-3.1-8b-instruct"))
+    register(Perplexity("llama-3.3-70b-instruct"))
 
 
 class PerplexityOptions(llm.Options):
